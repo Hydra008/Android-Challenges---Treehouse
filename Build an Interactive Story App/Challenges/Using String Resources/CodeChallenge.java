40896ebfd9baacc1d580743fd81a3360e628652b
Karan Shah <kshah2@lakeheadu.ca>
2017-10-06 18:14:59 -0300

    Update CodeChallenge.java

--- Build an Interactive Story App/Challenges/Using String Resources/CodeChallenge.java
+++ Build an Interactive Story App/Challenges/Using String Resources/CodeChallenge.java
@@ -1,3 +1,3 @@
 Button loadPuppiesButton = (Button) findViewById(R.id.puppiesButton);
-String buttonLabel = "";
+String buttonLabel = getString(R.string.morePuppies);
 loadPuppiesButton.setText("LOAD");
