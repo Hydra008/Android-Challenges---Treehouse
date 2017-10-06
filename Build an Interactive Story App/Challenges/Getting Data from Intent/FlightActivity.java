51305ae29441b4e504fa88354f876d8ca2b79c8e
Karan Shah <kshah2@lakeheadu.ca>
2017-10-06 17:43:12 -0300

    Create FlightActivity.java

---
+++ Build an Interactive Story App/Challenges/Getting Data from Intent/FlightActivity.java
@@ -0,0 +1,17 @@
+import android.os.Bundle;
+
+public class FlightActivity extends AppCompatActivity {
+    
+    public int fuelLevel = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight);
+    
+        // Add your code below!
+        Intent intent = getIntent();
+        
+    
+    }
+}
