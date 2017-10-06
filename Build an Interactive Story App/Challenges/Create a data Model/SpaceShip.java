1dad848b290815ca10294cf2de327985c39ad824
Karan Shah <kshah2@lakeheadu.ca>
2017-10-06 19:06:20 -0300

    Create SpaceShip.java

---
+++ Build an Interactive Story App/Challenges/Create a data Model/SpaceShip.java
@@ -0,0 +1,12 @@
+public class Spaceship {
+  public String shipType;
+  public String getShipType()
+ {
+   return shipType;
+ }
+  public void setShipType(String shipType)
+  {
+   this.shipType=shipType; 
+  }
+}
+ 
